f1906552aeb197bfcbfd382f1968f1c1a319faf2
Carlos Guevara <solrak.m@gmail.com>
2018-06-28 16:23:57 -0600

    Initial commit

---
+++ database/seeds/DatabaseSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // $this->call(UsersTableSeeder::class);
+        $threads=factory('App\Thread',50)->create();
+        $threads->each(function($thread){
+            {
+                factory('App\Reply',50)->create(['thread_id'=>$thread->id]);
+            }
+        });
+    }
+}
